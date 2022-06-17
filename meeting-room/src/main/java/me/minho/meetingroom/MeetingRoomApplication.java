747a9465a9c6332d45034743dd483e1940e1bbc5
5minho <5minhominho@gmail.com>
2022-06-17 22:30:42 +0900

    init commit

---
+++ meeting-room/src/main/java/me/minho/meetingroom/MeetingRoomApplication.java
@@ -0,0 +1,13 @@
+package me.minho.meetingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingRoomApplication.class, args);
+	}
+
+}
---
+++ meeting-room/src/test/java/me/minho/meetingroom/MeetingRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package me.minho.meetingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
